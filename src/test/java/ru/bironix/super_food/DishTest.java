2ce98e0fb512e46d5eeb047a4cfd6e2f691e51a8
Dmitry Andriyash <bironix17@outlook.com>
2022-03-28 16:39:18 +0300

    add test for dish and action

---
+++ src/test/java/ru/bironix/super_food/DishTest.java
@@ -0,0 +1,39 @@
+package ru.bironix.super_food;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.bironix.super_food.controllers.DishController;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Блюдо")
+public class DishTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Список блюд")
+    void getDishes() throws Exception {
+        this.mockMvc.perform(get("/dishes")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("id")));
+    }
+
+    @Test
+    @DisplayName("Конкретное блюдо")
+    void getDish() throws Exception {
+        this.mockMvc.perform(get("/dish/{id}", 0)).andExpect(status().isOk())
+                .andExpect(content().string(containsString("id")));
+    }
+}
--- src/test/java/ru/bironix/super_food/FoodApplicationTests.java
+++ src/test/java/ru/bironix/super_food/FoodApplicationTests.java
@@ -1,5 +1,6 @@
 package ru.bironix.super_food;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 class FoodApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
-
+    void contextLoads() {}
 }
