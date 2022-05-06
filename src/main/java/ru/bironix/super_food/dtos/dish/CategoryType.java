c25e9a1f011706f19d56022a95a09edfc25321f1
Dmitry Andriyash <bironix17@outlook.com>
2022-05-06 14:35:52 +0300

    create rus translation for categories

--- src/main/java/ru/bironix/super_food/dtos/dish/CategoryType.java
+++ src/main/java/ru/bironix/super_food/dtos/dish/CategoryType.java
@@ -1,8 +1,25 @@
 package ru.bironix.super_food.dtos.dish;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.annotations.ApiModel;
 
 @ApiModel
 public enum CategoryType {
-    BURGERS, ROLLS, PIZZA, DRINKS, SAUCES, COMBO
+    BURGERS("Бургеры"),
+    ROLLS("Роллы"),
+    PIZZA("Пицца"),
+    DRINKS("Напитки"),
+    SAUCES("Соусы"),
+    COMBO("Комбо");
+
+    private String rusName;
+
+    CategoryType(String rusName) {
+        this.rusName = rusName;
+    }
+
+    @JsonValue
+    public String getRusName() {
+        return rusName;
+    }
 }
