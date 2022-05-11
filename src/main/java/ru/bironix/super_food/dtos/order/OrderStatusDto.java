0ac58abf51e8ea39715c7080d1a90bd4ff77de93
Dmitry Andriyash <bironix17@outlook.com>
2022-05-11 11:41:06 +0300

    translate order status

--- src/main/java/ru/bironix/super_food/dtos/order/OrderStatusDto.java
+++ src/main/java/ru/bironix/super_food/dtos/order/OrderStatusDto.java
@@ -1,8 +1,27 @@
 package ru.bironix.super_food.dtos.order;
 
+import com.fasterxml.jackson.annotation.JsonValue;
 import io.swagger.annotations.ApiModel;
 
 @ApiModel
 public enum OrderStatusDto {
-    AWAITING_PAYMENT, EXPECTS, COOK, COOKED, DELIVERED, COMPLETED, CANCELED
+    AWAITING_PAYMENT("Ожидается оплата"),
+    EXPECTS("Ожидает исполнения"),
+    COOK("Готовится"),
+    COOKED("Готово"),
+    DELIVERING("Доставляется"),
+    COMPLETED("Завершен"),
+    CANCELED("Отменён");
+
+
+    private String rusName;
+
+    OrderStatusDto(String rusName) {
+        this.rusName = rusName;
+    }
+
+    @JsonValue
+    public String getRusName() {
+        return rusName;
+    }
 }
