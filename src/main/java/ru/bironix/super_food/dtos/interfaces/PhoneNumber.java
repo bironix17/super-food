127936558e9b255897a03e15452b26db4ec8792e
Dmitry Andriyash <bironix17@outlook.com>
2022-05-19 19:02:11 +0300

    add regexp Pattern for PhoneNumber

--- src/main/java/ru/bironix/super_food/dtos/interfaces/PhoneNumber.java
+++ src/main/java/ru/bironix/super_food/dtos/interfaces/PhoneNumber.java
@@ -1,8 +1,10 @@
 package ru.bironix.super_food.dtos.interfaces;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 public interface PhoneNumber {
     @NotBlank
+    @Pattern(regexp = "^(\\+7)[0-9]{10}$")
     String getPhoneNumber();
 }
