317a995657879b8fd91e713ca27b68e544ba2f9f
Dmitry Andriyash <bironix17@outlook.com>
2022-05-19 12:53:54 +0300

    fix notnull in BindDishForOrder

--- src/main/java/ru/bironix/super_food/dtos/interfaces/dish/BindDishForOrder.java
+++ src/main/java/ru/bironix/super_food/dtos/interfaces/dish/BindDishForOrder.java
@@ -3,9 +3,10 @@ package ru.bironix.super_food.dtos.interfaces.dish;
 import ru.bironix.super_food.dtos.dish.DishDto;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 public interface BindDishForOrder {
-
     @Valid
+    @NotNull
     DishDto.BindForOrder getDish();
 }
